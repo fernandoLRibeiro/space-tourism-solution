80536e5bce0e641e8bf2f7fb47f8972566a3874c
fernandoLRibeiro <fernandolribeiro@protonmail.com>
2022-05-26 20:25:58 -0300

    Set up styled-components

---
+++ app/components/Typography/Typography.styled.js
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+export const Heading1 = styled.h1`
+  font-family: ${({ theme }) => theme.font.fontFamily.serif};
+  font-size: ${({ theme }) => theme.font.fontSize.lagest};
+  font-weight: 400;
+`;
+
+export const Heading2 = styled.h2`
+  font-family: ${({ theme }) => theme.font.fontFamily.serif};
+  font-size: ${({ theme }) => theme.font.fontSize.xxxLarge};
+  font-weight: 400;
+`;
+
+export const Heading3 = styled.h3`
+  font-family: ${({ theme }) => theme.font.fontFamily.serif};
+  font-size: ${({ theme }) => theme.font.fontSize.xxLarge};
+  font-weight: 400;
+`;
+
+export const Heading4 = styled.h4`
+  font-family: ${({ theme }) => theme.font.fontFamily.serif};
+  font-size: ${({ theme }) => theme.font.fontSize.xLarge};
+  font-weight: 400;
+`;
+
+export const Heading5 = styled.h5`
+  font-family: ${({ theme }) => theme.font.fontFamily.sansSerifCondensed};
+  font-size: ${({ theme }) => theme.font.fontSize.large};
+  font-weight: 400;
+  letter-spacing: ${({ theme }) => theme.font.letterSpacing.heading5};
+`;
+
+export const Subheading1 = styled.h5`
+  font-family: ${({ theme }) => theme.font.fontFamily.serif};
+  font-size: ${({ theme }) => theme.font.fontSize.large};
+  font-weight: 400;
+`;
+
+export const Subheading2 = styled.h6`
+  font-family: ${({ theme }) => theme.font.fontFamily.sansSerifCondensed};
+  font-size: ${({ theme }) => theme.font.fontSize.xSmall};
+  font-weight: 400;
+  letter-spacing: ${({ theme }) => theme.font.letterSpacing.subheading2};
+`;
+
+export const NavText = styled.p`
+  font-family: ${({ theme }) => theme.font.fontFamily.sansSerifCondensed};
+  font-size: ${({ theme }) => theme.font.fontSize.small};
+  font-weight: 400;
+  letter-spacing: ${({ theme }) => theme.font.letterSpacing.navText};
+`;
+
+export const BodyText = styled.p`
+  font-family: ${({ theme }) => theme.font.fontFamily.sansSerifCondensed};
+  font-size: ${({ theme }) => theme.font.fontSize.regular};
+  font-weight: 400;
+`;
--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,7 +1,63 @@
-import '../styles/globals.css'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import Layout from "../components/Layout/Layout";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+const theme = {
+  colors: {
+    black: "#0B0D17",
+    grey: "#616476",
+    lightBlue: "#D0D6F9",
+    white: "#FFFFFF",
+  },
+  font: {
+    fontFamily: {
+      // Heading 1, Heading 2, Heading 3, Heading 4, Subheading 1
+      serif: `'Bellefair', serif`,
+      // Heading 5, Subheading2, Nav Text
+      sansSerifCondensed: `'Barlow Condensed', sans-serif`,
+    },
+    fontSizes: {
+      // Heading 1 desktop
+      largest: "9.375em",
+      // Heading 2 Desktop
+      xxxLarge: "6.25em",
+      // Heading 3 Desktop
+      xxLarge: "3.5em",
+      // Heading 4 desktop
+      xLarge: "2em",
+      // Heading 5 Desktop, Subheading 1 desktop
+      large: "1.75em",
+      // Body Text desktop
+      regular: "1.125em",
+      // Nav Text desktop
+      small: "1em",
+      // Subheading 2 desktop
+      xSmall: "0.875em",
+    },
+    letterSpacing: {
+      heading5: "4.75px",
+      subheading2: "2.35px",
+      navText: "2.7px",
+    },
+  },
+};
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </>
+  );
 }
-
-export default MyApp
