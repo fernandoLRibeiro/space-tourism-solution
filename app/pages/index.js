dbd0eb53dbbc77132d2570e913aef50ff01a0f37
fernandoLRibeiro <fernandolribeiro@protonmail.com>
2022-06-08 23:58:23 -0300

    Complete home page desktop design

--- app/pages/index.js
+++ app/pages/index.js
@@ -8,6 +8,22 @@ export default function Home() {
       <Head>
         <title>Home</title>
       </Head>
+      <main className={styles.mainContent}>
+        <div className={styles.textContainer}>
+          <h5 className="heading5">SO, YOU WANT TO TRAVEL TO</h5>
+          <h1 className="heading1">SPACE</h1>
+
+          <p className="bodyText">
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+        <button className={styles.exploreButton}>
+          <h4 className="heading4">EXPLORE</h4>
+        </button>
+      </main>
     </div>
   );
 }
